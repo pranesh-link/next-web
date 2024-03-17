77f94cf6ab5f78d8e5db4af42075cbcd552f7d04
Pranesh <prans1991@gmail.com>
2024-03-17 22:38:42 +0530

    Enable user scalable prop, add robots.txt and upgrade version to 2.0.0

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,7 @@ export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
   maximumScale: 1,
-  userScalable: false,
+  userScalable: true,
 };
 
 export default async function RootLayout({
