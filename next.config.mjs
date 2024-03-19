f41382ef8579bb7a1e9fc429019b1fed0e3ec541
Pranesh <prans1991@gmail.com>
2024-03-19 19:41:33 +0530

    Add sharp module for image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 import packageJson from "./package.json" assert { type: "json" };
 const nextConfig = {
-  redirects: async () => [
-    // {
-    //   source: "/bmi:slug*",
-    //   destination: "/tools/bmi-calculator",
-    //   permanent: true,
-    // },
-  ],
   compiler: {
     styledComponents: true,
   },
