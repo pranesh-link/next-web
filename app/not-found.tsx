bd510bfcd3c456225fdf8ee6710f4f330fcde39d
Pranesh <prans1991@gmail.com>
2024-04-29 15:17:57 +0530

    Change NotFound to a dynamic component

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,73 +1,12 @@
 "use client";
-import { useContext, useEffect, useMemo, useRef, useState } from "react";
-import styled from "styled-components";
-import { AppContext } from "@/_store/app/context";
-import { FlexBox } from "@/_components/common/Elements";
-import { ROUTES } from "@/_constants/common";
-import { redirect } from "next/navigation";
+import dynamic from "next/dynamic";
+
+const DynamicNotFound = dynamic(() => import("./_components/NotFound"), {
+  ssr: false,
+});
 
 const NotFoundPage = () => {
-  const {
-    data: {
-      appConfig: {
-        notFoundPage: { title },
-        labels,
-      },
-    },
-  } = useContext(AppContext);
-
-  const [counter, setCounter] = useState(5);
-  const [timer, updateTimer] = useState<any>("");
-
-  const redirectMessage = useMemo(() => {
-    return labels.goToHomePage.replace("{0}", `${counter}`);
-  }, [counter, labels.goToHomePage]);
-
-  useEffect(() => {
-    if (counter === 0) {
-      redirect(ROUTES.ROUTE_HOME);
-    }
-  }, [counter]);
-
-  useEffect(() => {
-    (() => {
-      clearInterval(timer);
-      const timerId = setInterval(() => {
-        setCounter((prevCounter) => prevCounter - 1);
-      }, 1000);
-      updateTimer(timerId);
-    })();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  const showContent = useMemo(() => title, [title]);
-  return showContent ? (
-    <NotFoundPageWrapper $direction="column" $alignItems="center">
-      <NotFoundTitle dangerouslySetInnerHTML={{ __html: title }} />
-      <p className="redirecting-you">{redirectMessage}</p>
-    </NotFoundPageWrapper>
-  ) : null;
+  return <DynamicNotFound />;
 };
 
 export default NotFoundPage;
-
-const NotFoundPageWrapper = styled(FlexBox)`
-  position: fixed;
-  height: 100%;
-  width: 100%;
-  top: 0;
-  background: #faf6f9;
-  .redirecting-you {
-    font-size: 16px;
-    font-weight: 500;
-  }
-`;
-
-const NotFoundTitle = styled.div`
-  margin: 0 auto;
-  margin-top: 50px;
-  text-align: center;
-  h1 {
-    color: #ee4b2b;
-  }
-`;
