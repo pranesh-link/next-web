c83bf29eef67acbef7ceac5a24841ebfd1ab1d40
Pranesh <prans1991@gmail.com>
2024-05-07 15:49:22 +0530

    Add dynamic imports for pages

--- app/maintenance/page.tsx
+++ app/maintenance/page.tsx
@@ -1,82 +1,13 @@
 "use client";
-import MaintenanceAnimation from "@/_assets/maintenance.gif";
-import LazyLoadedImage from "@/_components/common/LazyLoadedImage";
-import Contact from "@/_components/profile/sections/Contact";
-import { AppContext } from "@/_store/app/context";
-import { useContext, useRef } from "react";
-import styled from "styled-components";
+import dynamic from "next/dynamic";
+
+const DynamicMaintenanceC = dynamic(
+  () => import("@/_components/maintenance/Maintenance"),
+  { ssr: false }
+);
 
 const MaintenancePage = () => {
-  const {
-    data: {
-      links,
-      currentDevice: { isMobile },
-      maintenance,
-    },
-  } = useContext(AppContext);
-
-  return (
-    <MaintenanceArticle $isMobile={isMobile}>
-      <div className="maintenance-info">
-        <LazyLoadedImage
-          className="maintenance-image"
-          height={isMobile ? 300 : 400}
-          alt="maintenance"
-          src={MaintenanceAnimation}
-          unoptimized
-        />
-        <h1 dangerouslySetInnerHTML={{ __html: maintenance.message }} />
-      </div>
-      <div className="contact-links">
-        <Contact links={links} refObj={useRef(null)} />
-      </div>
-    </MaintenanceArticle>
-  );
+  return <DynamicMaintenanceC />;
 };
-
-const MaintenanceArticle = styled.article<{ $isMobile: boolean }>`
-  display: block;
-  height: 100vh;
-  overflow: hidden;
-  padding: 0;
-  background: #d6433b;
-  color: #fff;
-
-  .maintenance-image {
-    height: ${(props) => (props.$isMobile ? "300px" : "400px")};
-  }
-
-  .contact-links {
-    position: fixed;
-    margin: 0 auto;
-    width: 100%;
-    bottom: 0px;
-    padding: 20px 0;
-    background: rgb(34, 34, 34);
-    .links {
-      justify-content: space-evenly;
-    }
-    img {
-      height: 30px;
-      margin-right: ${(props) => (props.$isMobile ? "0" : "50px")};
-    }
-  }
-
-  .maintenance-info {
-    margin: 0 auto;
-    padding: 50px;
-    text-align: center;
-  }
-
-  p {
-    font-size: 20px;
-  }
-
-  h1 {
-    font-size: ${(props) => (props.$isMobile ? "30px" : "50px")};
-    font-weight: 100;
-    text-align: center;
-  }
-`;
 
 export default MaintenancePage;
