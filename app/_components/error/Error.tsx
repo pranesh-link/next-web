b9e87d9d9ea9d14ce3dfed6bbdd67719efcc3b1e
Pranesh <prans1991@gmail.com>
2024-05-07 11:05:43 +0530

    Add retrying shimmer animation

--- app/_components/error/Error.tsx
+++ app/_components/error/Error.tsx
@@ -1,7 +1,8 @@
 import { LABEL_TEXT, MESSAGES } from "@/_constants/profile";
 import { AppContext } from "@/_store/app/context";
+import classNames from "classnames";
 import { useRouter } from "next/navigation";
-import { startTransition, useContext } from "react";
+import { startTransition, useContext, useState } from "react";
 import styled from "styled-components";
 import { ActionBtn, FlexBoxSection } from "../common/Elements";
 
@@ -12,25 +13,32 @@ function Error({ reset }: { reset: () => void }) {
     },
   } = useContext(AppContext);
   const router = useRouter();
+  const [retrying, setRetrying] = useState(false);
 
   return (
     <ErrorWrapper
+      className={classNames({ retrying: retrying })}
       $direction="column"
       $justifyContent={isMobile ? "center" : "flex-start"}
       $alignItems="center"
     >
-      <h2>{MESSAGES.genericError}</h2>
-      <ActionBtn
-        className="retry"
-        onClick={() => {
-          const url = new URL(window.location.href);
-          router.push(url.origin + url.pathname);
-          router.refresh();
-          startTransition(() => reset());
-        }}
-      >
-        {LABEL_TEXT.retry}
-      </ActionBtn>
+      <div className={classNames("error-content", { retrying: retrying })}>
+        <h2>{MESSAGES.genericError}</h2>
+        <ActionBtn
+          className="retry"
+          onClick={() => {
+            setRetrying(true);
+            const url = new URL(window.location.href);
+            router.push(url.origin + url.pathname);
+            router.refresh();
+            startTransition(() => {
+              reset();
+            });
+          }}
+        >
+          {retrying ? LABEL_TEXT.retrying : LABEL_TEXT.retry}
+        </ActionBtn>
+      </div>
     </ErrorWrapper>
   );
 }
@@ -42,6 +50,40 @@ const ErrorWrapper = styled(FlexBoxSection)`
   color: #f0f0f0;
   height: 100vh;
   text-align: center;
+  padding: 15px;
+  &.retrying {
+    background: #f0f0f0;
+  }
+
+  .error-content {
+    padding: 15px;
+    height: 100%;
+    &.retrying {
+      background: #f0f0f0;
+      border-radius: 15px;
+      background: linear-gradient(
+        -45deg,
+        #ffa500 40%,
+        #ffa500 50%,
+        #f7ba48 60%
+      );
+      box-shadow: transparent 0px -1px 0px 0px,
+        rgba(240, 240, 240, 0.3) 0px -1px 0px inset, #ccc 0px 2px 12px;
+      background-size: 300%;
+      background-position-x: 100%;
+      animation: shimmer 2.5s infinite linear;
+      @keyframes shimmer {
+        to {
+          background-position-x: 0%;
+        }
+      }
+      .retry {
+        &:hover {
+          background: #3498db;
+        }
+      }
+    }
+  }
   h2 {
     font-size: 32px;
     padding-top: 10%;
