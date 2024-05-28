4fb0033ce2f29e098e8c44d9476ecc2bc0ae03da
Pranesh <prans1991@gmail.com>
2024-05-28 18:15:43 +0530

    Add MongoDB data fetch for maintenance config

--- app/api/maintenance/route.ts
+++ app/api/maintenance/route.ts
@@ -1,13 +1,19 @@
-import { CORS_MODE } from "@/_constants/profile";
+import connectDB from "@/config/database";
+import MaintenanceModel from "@/models/maintenanceModel";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const maintenance = await (
-    await fetch(`${process.env.NEXT_PUBLIC_CMS_SERVER}/maintenance.json`, {
-      mode: CORS_MODE,
-      cache: "no-store",
-    })
-  ).json();
-
-  return NextResponse.json(maintenance);
+  // const maintenance = await (
+  //   await fetch(`${process.env.NEXT_PUBLIC_CMS_SERVER}/maintenance.json`, {
+  //     mode: CORS_MODE,
+  //     cache: "no-store",
+  //   })
+  // ).json();
+  await connectDB();
+  try {
+    const data = await MaintenanceModel.findOne();
+    return NextResponse.json(JSON.parse(JSON.stringify(data)));
+  } catch (error) {
+    return NextResponse.json({ msg: "GET", error });
+  }
 }
