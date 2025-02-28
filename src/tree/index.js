81c08616d0df9541f8b9c97c400b1e120e82f7b4
Pranesh G <prans1991@gmail.com>
2025-02-28 22:36:46 +0530

    Add styling to replace nbsp

--- src/tree/index.js
+++ src/tree/index.js
@@ -1,16 +1,29 @@
-import React from 'react';
-import './index.css'
+import React from "react";
+import "./index.css";
 
 export default function Tree() {
-    return (
+  return (
+    <>
       <div className="tree">
-        mammals<br/>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;cheetah <br/>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;bear <br/>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;lion <br/>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;dog <br/>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;elephant <br/>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ape <br/>
+        mammals
+        <br />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;cheetah <br />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;bear <br />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;lion <br />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;dog <br />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;elephant{" "}
+        <br />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ape <br />
       </div>
-    )
+      <div className="tree">
+        <div>mammals</div>
+        <div className="first-spacing">cheetah</div>
+        <div className="first-spacing">bear</div>
+        <div className="second-spacing">lion</div>
+        <div className="second-spacing">dog</div>
+        <div className="third-spacing">elephant</div>
+        <div className="first-spacing">ape</div>
+      </div>
+    </>
+  );
 }
