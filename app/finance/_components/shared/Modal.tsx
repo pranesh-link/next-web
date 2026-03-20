b20afc4b3f46c1cf0e5cbe8d3df10ff5589badc2
Pranesh G <prans1991@gmail.com>
2026-03-20 21:27:51 +0530

    Refactor import order in Modal component for consistency

--- app/finance/_components/shared/Modal.tsx
+++ app/finance/_components/shared/Modal.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect, useRef, useCallback } from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 import styled, { keyframes } from 'styled-components';
 
